7b30b0fde85e8bbbd323ec3f8d02fe397a93064a
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-16 23:22:51 +0900

    Brew formula update for colout version v0.1.1

--- colout.rb
+++ colout.rb
@@ -7,16 +7,16 @@ class Colout < Formula
 
   if OS.mac?
     url "https://github.com/kemokemo/colout/releases/download/v0.1.1/colout_Darwin_x86_64.tar.gz"
-    sha256 "b8cadd17801915ade024168f166941d8e04e829662d1c5300bc724f885719841"
+    sha256 "196b1cd82f6c254d621aec96008521099bf7edba5bdcb7442bcb5c0b3c1fbe40"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/kemokemo/colout/releases/download/v0.1.1/colout_Linux_x86_64.tar.gz"
-      sha256 "b702d216c53de8879f1fb9c91235259df574505a33a444071f2d266a33c00275"
+      sha256 "742455422d9008c6cc7db9021e13e0d4161e3e3fac8acc1e25ac99e28ba40156"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/kemokemo/colout/releases/download/v0.1.1/colout_Linux_arm64.tar.gz"
-        sha256 "c6138ed3aa1cbb235c4c553488e6f125e7a25414260ad178a43d6b9965259488"
+        sha256 "702b31bc86383f1b997e4c7f1311978037630e2f36ec4047d6b554cb41e6ca02"
       else
       end
     end
