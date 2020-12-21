31bbed5ca5c61e938ecbefea425423e2db68b89b
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-22 00:16:33 +0900

    Brew formula update for gomrepo version v0.1.0

--- gomrepo.rb
+++ gomrepo.rb
@@ -7,15 +7,15 @@ class Gomrepo < Formula
 
   if OS.mac?
     url "https://github.com/kemokemo/gomrepo/releases/download/v0.1.0/gomrepo_Darwin_x86_64.tar.gz"
-    sha256 "87495c233334b00ac7b7579a768741ba4d66e1a08b9f0e4d67bda297a08f9b7b"
+    sha256 "4626462b90388532293d80061c4a5c1b670d4a0cff0d11e64ef0bccdb81b54a2"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/kemokemo/gomrepo/releases/download/v0.1.0/gomrepo_Linux_x86_64.tar.gz"
-    sha256 "cb61240a1b91d29ce9c7d172b33ebfab333117b9573ecceb715c55f58612dbcd"
+    sha256 "9640829cb1593540b36c5b5595bd121ff38be2001a146dfea2e3a1ebc325ec46"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/kemokemo/gomrepo/releases/download/v0.1.0/gomrepo_Linux_arm64.tar.gz"
-    sha256 "7812f4c82769a0307db787afc25d610f3ab1637fcc9307768007376238b347b4"
+    sha256 "b4d9a1a8a43271dfc9bb792d7def0dcd06878b55dd656f0cd304537a0923144b"
   end
 
   def install
